fa3a3a010f8edebe402c15c1bf792cb3ac60807e
ESsEr12 <104263815+ESsEr12@users.noreply.github.com>
2022-08-26 13:55:41 +0300

    Add files via upload

---
+++ програ/1.js
@@ -0,0 +1,28 @@
+//let a=;
+//let (b=number("23sdf"))
+//let (c=parselnt("23.5px"))
+//let (d=iho reihori("23.yttg"))
+//alert(a+d)
+
+//let w="5.5px",h="6.25px"
+//a=parseInt(w)
+//b=parseFloat(h)
+//alert(a+b+"px")
+
+//let a="бронетранстёр"
+//alert(a[3]+a[2]+a[6]+a[7])
+
+//let a
+//let b
+//a=prompt("ndjsdofsfsfsgsg")
+//b=prompt("etertetete")
+//alert(a+""+b)
+
+
+/*let a
+let b=[0,1,3,4,]
+b[0]=1
+b[5]=5
+b.push(6)
+*/
+alert("")
---
+++ програ/2.js
@@ -0,0 +1,22 @@
+function Treaure(){
+	alert("dfdg")
+	alert("dff")
+	let name=prompt("dggdg")
+	while name===""{
+		alert("fxdfdffg")
+		name=promt("dfdfgdgds")}
+		alert("dfvggfdudfydf"+name)
+		alert("dfdfdfdgdf")
+		let (thing===""){
+			("dsdfsfsddf")
+		}
+		if(thing==""){alert("dffddgdg")}
+		else{alert("fgdgdggd"+thing+"rrtgrgrtg")}
+	
+
+
+
+
+
+
+}
